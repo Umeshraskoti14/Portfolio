22ab3987f418fe95596425f7749e4f51ae24d216
Umeshraskoti14 <umeshraskoti142gmail.com>
2026-04-19 16:06:37 +0545

    commit all changes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,10 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "./components/Layout";
 import { HomePage } from "./pages/HomePage";
-import { AboutPage } from "./pages/AboutPage";
-import { PortfolioPage } from "./pages/PortfolioPage";
-import { SkillsPage } from "./pages/SkillsPage";
-import { ContactPage } from "./pages/ContactPage";
 
 export const router = createBrowserRouter([
   {
@@ -12,10 +8,6 @@ export const router = createBrowserRouter([
     Component: Layout,
     children: [
       { index: true, Component: HomePage },
-      { path: "about", Component: AboutPage },
-      { path: "portfolio", Component: PortfolioPage },
-      { path: "skills", Component: SkillsPage },
-      { path: "contact", Component: ContactPage },
     ],
   },
 ]);
